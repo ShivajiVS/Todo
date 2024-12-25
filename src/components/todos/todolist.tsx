ff96b5cd330d0214f409be906be6b12300575ed5
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-12-25 16:46:14 +0530

    handling if there is no todos

--- src/components/todos/todolist.tsx
+++ src/components/todos/todolist.tsx
@@ -2,7 +2,7 @@ import { Todo } from "@/lib/localstorage";
 import { Card } from "../ui/card";
 import { Button } from "../ui/button";
 import { Link } from "react-router";
-import { Ellipsis, EllipsisVertical } from "lucide-react";
+import { EllipsisVertical } from "lucide-react";
 import { useState } from "react";
 
 export const Todolist = ({
@@ -20,7 +20,14 @@ export const Todolist = ({
     setVisibleMenuId((prevId) => (prevId === id ? null : id));
   };
 
-  if (todos.length === 0) return <div>no todos</div>;
+  if (todos.length === 0)
+    return (
+      <div className="flex items-center justify-center w-full h-[calc(100vh-250px)] mt-10">
+        <h3 className="text-xl font-semibold tracking-tight capitalize">
+          You have no todos, please add
+        </h3>
+      </div>
+    );
 
   return (
     <section className="flex flex-col w-full max-w-4xl px-1 pb-20 mt-10 space-y-4 lg:mx-auto">
