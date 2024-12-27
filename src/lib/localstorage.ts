ecab78ab0eb651552bcefe855a3f50eba8831424
Kondeti Sivaji <shivajikondeti555@gmail.com>
2024-12-27 23:27:54 +0530

    added local storage helpers for authentication

--- src/lib/localstorage.ts
+++ src/lib/localstorage.ts
@@ -4,6 +4,13 @@ export interface Todo {
   description: string;
 }
 
+export interface UserType {
+  id: string;
+  fullName: string;
+  email: string;
+  password: string;
+}
+
 export const getTodos = (): Todo[] => {
   const todos = localStorage.getItem("todos");
   return todos ? (JSON.parse(todos) as Todo[]) : [];
@@ -16,4 +23,13 @@ export const saveTodos = (todos: Todo[]): void => {
 export const getTodoById = (id: string): Todo | undefined => {
   const todos = getTodos();
   return todos.find((todo) => todo.id === id);
+};
+
+export const createUser = (): UserType | undefined => {
+  const user = localStorage.getItem("user");
+  return user ? (JSON.parse(user) as UserType) : undefined;
+};
+
+export const getUser = (user: UserType) => {
+  localStorage.setItem("user", JSON.stringify(user));
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
       <div className="flex flex-col space-y-8">
-        <h1 className="text-6xl font-bold tracking-tight text-orange-500">
-          welcome to Social
+        <h1 className="text-6xl font-bold tracking-tight capitalize">
+          welcome to{" "}
+          <span className="text-orange-500 border-b-2">Todolist</span>
         </h1>
         <Link to="/sign-in" className="mt-4 text-lg text-center ">
           <Button size="lg">LogIn</Button>
